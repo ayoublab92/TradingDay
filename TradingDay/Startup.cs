e70994361b662ca59d851bdc59f506280fa87524
Chedy Missaoui <missaoui.chedy@gmail.com>
2018-08-08 20:12:38 +0100

    Added a comment on Startup.cs

--- TradingDay/Startup.cs
+++ TradingDay/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+// Bich nzid commantaire
 namespace TradingDay
 {
     public class Startup
