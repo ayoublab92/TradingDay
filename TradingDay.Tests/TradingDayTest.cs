2389ba7e1aaba920ef6af9d2821c6add18c580f6
Chedy Missaoui <missaoui.chedy@gmail.com>
2018-08-19 11:14:40 +0100

    Added Some Targets to the MSBUILD file

---
+++ TradingDay.Tests/TradingDayTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TradingDay.Tests
+{
+    [TestClass]
+    public class TradingDayTest
+    {
+        [TestMethod]
+        public void ShouldFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
